9d3f8106db492fbe1095a6b6de2af88d43b5c7e0
dhankutey <kaushaleyy@gmail.com>
2026-06-12 20:20:30 +0545

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'project-cache-v1';
+// Add the paths to all the files you want available offline
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',     // Change to your actual CSS file name
+  './script.js',    // Change to your actual JS file name
+  './icon.png'       // Change to your actual icon name
+];
+
+// Install the service worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate worker and clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch assets from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
