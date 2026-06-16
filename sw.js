2b21a5fe35d068886ff5911171868949a4b7d5a3
dhankutey <kaushaleyy@gmail.com>
2026-06-16 16:20:32 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,22 @@
-const CACHE_NAME = 'bingo-cache-v3'; 
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './index.css',
-  './index.js',
-  './manifest.json'
-];
+const CACHE_NAME = 'bingo-v1';
+const ASSETS = ['./', './index.html', './index.css', './index.js', './manifest.json'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    }).then(() => self.skipWaiting())
-  );
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
