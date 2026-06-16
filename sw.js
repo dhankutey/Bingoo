8b73bae0c189d9bad1db78e7a3b657273904b01f
dhankutey <kaushaleyy@gmail.com>
2026-06-16 13:07:11 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,38 @@
-const CACHE_NAME = 'bingo-cache-v2'; // Bumped version to force an update
-const ASSETS_TO_CACHE = [
-  '/bingo/',
-  '/bingo/index.html',
-  '/bingo/index.css',
-  '/bingo/index.js',
-  '/bingo/manifest.json',
-  '/bingo/icon.png'
+const CACHE_NAME = 'bingo-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './index.css',
+  './index.js',
+  './manifest.json'
 ];
 
-// Install the service worker and cache assets
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    }).then(() => self.skipWaiting()) // Forces the waiting service worker to become active
-  );
-});
-
-// Activate worker and clean up old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim()) // Forces the service worker to take control immediately
-  );
-});
-
-// Fetch assets from cache if offline
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Return cache match, otherwise fetch from network
-      return response || fetch(event.request);
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
 
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
